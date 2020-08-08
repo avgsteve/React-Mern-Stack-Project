daad51b2f73da14d4c6c28c1d441db952bb12720
avgsteve <steve.leng@gmail.com>
2020-08-08 12:51:34 +0800

    Update register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -4,59 +4,112 @@ import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
+import Axios from 'axios';
 
-const Register = ( { setAlert, register, isAuthenticated } ) => {
-  const [formData, setFormData] = useState( { name: '', email: '', password: '', password2: '' } );
+const Register = ({ setAlert, register, isAuthenticated }) => {
 
-  const { name, email, password, password2 } = formData;
+  const [data_in_form, set_data_in_Form] = useState(
+    // initial state of forData
+    { name: '', email: '', password: '', password_confirm: '' }
+  );
 
-  const onChange = ( e ) => setFormData( {
-    ...formData,
-    [ e.target.name ]: e.target.value
-  } );
+  //  
+  const { name, email, password, password_confirm } = data_in_form;
 
-  const onSubmit = async ( e ) => {
+
+  // change the value (state) of variables for form data
+  const onChange = (e) => set_data_in_Form({
+    ...data_in_form,
+    // In the Array, use the name , which is the name attribute from DOM element , dynamically changed as item to store data
+    [e.target.name]: e.target.value,
+
+  });
+
+  // // for testing
+  // if (password_confirm)
+  //   setTimeout(
+  //     () => { 
+  //       if (password !== password_confirm) {
+  //         console.log('Passwords do not match');
+  //       }
+  //     }, 1000);
+
+  const onSubmit = async (e) => {
     e.preventDefault();
-    if ( password !== password2 ) {
-      setAlert( 'Passwords do not match', 'danger' );
+    if (password !== password_confirm) {
+      setAlert('Passwords do not match', 'danger');
     } else {
-      register( { name, email, password } );
+
+      register({ name, email, password });
+
+      // //  ===== for testing ====
+      // const newUserData = { name, email, password };
+
+      // // fetch token here
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   };
+      //   const body = JSON.stringify(newUserData);
+      //   const res = await Axios.post('/api/users', body, config);
+      //   console.log(res.data);
+      // } catch (err) {
+      //   console.log(`There's an error while registering new user`, err);
+      // }
+      // //  ===== for testing ====
+
     }
   };
 
-  if ( isAuthenticated ) {
-    return <Redirect to="/dashboard"/>;
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
   }
 
-  return ( <Fragment>
+  return (<Fragment>
+
     <h1 className="large text-primary">Sign Up</h1>
     <p className="lead">
-      <i className="fas fa-user"/>
+      <i className="fas fa-user" />
       Create Your Account
     </p>
+
+
     <form className="form" onSubmit={onSubmit}>
+
+      {/* CSS stlye:  .form .form-group {  margin: 1.2rem 0;} 0; */}
+
       <div className="form-group">
-        <input type="text" placeholder="Name" name="name" value={name} onChange={onChange}/>
+        <input type="text" placeholder="Name" name="name" value={name} onChange={onChange} />
       </div>
+
       <div className="form-group">
-        <input type="email" placeholder="Email Address" name="email" value={email} onChange={onChange}/>
+        <input type="email" placeholder="Email Address" name="email" value={email} onChange={onChange} />
         <small className="form-text">
           This site uses Gravatar so if you want a profile image, use a Gravatar email
         </small>
       </div>
+
       <div className="form-group">
-        <input type="password" placeholder="Password" name="password" value={password} onChange={onChange}/>
+        <input type="password" placeholder="Password" name="password" value={password} onChange={onChange} />
       </div>
+
       <div className="form-group">
-        <input type="password" placeholder="Confirm Password" name="password2" value={password2} onChange={onChange}/>
+        <input type="password" placeholder="Confirm Password" name="password_confirm" value={password_confirm} onChange={onChange} />
       </div>
-      <input type="submit" className="btn btn-primary" value="Register"/>
+
+      <input type="submit" className="btn btn-primary" value="Register" />
+
     </form>
+
+
     <p className="my-1">
       Already have an account?
       <Link to="/login">Sign In</Link>
     </p>
-  </Fragment> );
+
+  </Fragment>);
 };
 
 Register.propTypes = {
@@ -65,6 +118,6 @@ Register.propTypes = {
   isAuthenticated: PropTypes.bool
 };
 
-const mapStateToProps = ( state ) => ( { isAuthenticated: state.auth.isAuthenticated } );
+const mapStateToProps = (state) => ({ isAuthenticated: state.auth.isAuthenticated });
 
-export default connect( mapStateToProps, { setAlert, register } )( Register );
+export default connect(mapStateToProps, { setAlert, register })(Register);
